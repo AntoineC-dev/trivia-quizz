8e92a9dac3b38646c04d62e28a118e9d6f5d9f7b
Antoine Cheminat <ac.dev@hotmail.com>
2022-04-06 18:25:42 -0400

    Add Router to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import * as React from "react";
-import { ChakraProvider, VStack, Grid, theme, Heading } from "@chakra-ui/react";
-import { ColorModeSwitcher } from "./components";
+import { ChakraProvider, theme } from "@chakra-ui/react";
+import { Router } from "./navigation";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Grid minH="100vh" p={3}>
-      <ColorModeSwitcher justifySelf="flex-end" />
-      <VStack spacing={8}>
-        <Heading as="h1">Quizz</Heading>
-      </VStack>
-    </Grid>
+    <Router />
   </ChakraProvider>
 );
