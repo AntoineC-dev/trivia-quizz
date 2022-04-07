672315aa7ce94cc672d6871cd489905ac2942424
Antoine Cheminat <ac.dev@hotmail.com>
2022-04-07 15:08:31 -0400

    Extract Layout

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
-import { Button, Grid, Heading, VStack } from "@chakra-ui/react";
+import { Button, Heading, VStack } from "@chakra-ui/react";
 import {
-  ColorModeSwitcher,
   QuestionAmountInput,
   QuestionCategorySelect,
   QuestionDifficultyRadio,
@@ -11,30 +10,27 @@ import { ArrowForwardIcon } from "@chakra-ui/icons";
 
 export const Home = () => {
   return (
-    <Grid minH="100vh" p={3} textAlign="center">
-      <ColorModeSwitcher justifySelf="flex-end" />
-      <VStack spacing={8} w="100%" maxW="lg" justifySelf="center">
-        <VStack spacing={4}>
-          <Heading as="h1" size="3xl">
-            Trivia Quizz
-          </Heading>
-          <Heading as="h2" size="md" lineHeight={1.4}>
-            The Open Trivia Database provides a completely free JSON API for use in programming projects.
-          </Heading>
-        </VStack>
-        <QuestionInputGroup title="Number of question(s):">
-          <QuestionAmountInput />
-        </QuestionInputGroup>
-        <QuestionInputGroup title="Category:">
-          <QuestionCategorySelect />
-        </QuestionInputGroup>
-        <QuestionInputGroup title="Difficulty:">
-          <QuestionDifficultyRadio />
-        </QuestionInputGroup>
-        <Button size="lg" rightIcon={<ArrowForwardIcon />}>
-          Start the quizz
-        </Button>
+    <VStack spacing={8} w="100%" maxW="lg" justifySelf="center">
+      <VStack spacing={4}>
+        <Heading as="h1" size="3xl">
+          Trivia Quizz
+        </Heading>
+        <Heading as="h2" size="md" lineHeight={1.4}>
+          The Open Trivia Database provides a completely free JSON API for use in programming projects.
+        </Heading>
       </VStack>
-    </Grid>
+      <QuestionInputGroup title="Number of question(s):">
+        <QuestionAmountInput />
+      </QuestionInputGroup>
+      <QuestionInputGroup title="Category:">
+        <QuestionCategorySelect />
+      </QuestionInputGroup>
+      <QuestionInputGroup title="Difficulty:">
+        <QuestionDifficultyRadio />
+      </QuestionInputGroup>
+      <Button size="lg" rightIcon={<ArrowForwardIcon />}>
+        Start the quizz
+      </Button>
+    </VStack>
   );
 };
