cc7905528f479fee483a605ebe502efe0a6dfa30
Antoine Cheminat <ac.dev@hotmail.com>
2022-04-07 01:48:12 -0400

    Export newly created components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,4 @@
 export * from "./colorModeSwitcher/ColorModeSwitcher";
+export * from "./questionAmountInput/QuestionAmountInput";
+export * from "./questionCategorySelect/QuestionCategorySelect";
+export * from "./questionDifficultyRadio/QuestionDifficultyRadio";
