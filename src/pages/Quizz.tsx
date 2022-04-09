78548edfa38b538d45e6a2754060a9638cd989e3
Antoine Cheminat <ac.dev@hotmail.com>
2022-04-08 23:20:06 -0400

    Remove initializeQuizz useEffectr

--- src/pages/Quizz.tsx
+++ src/pages/Quizz.tsx
@@ -1,17 +1,13 @@
 import * as React from "react";
 import { Heading, Spinner, Text, VStack } from "@chakra-ui/react";
-import { useInitializeQuizz, useQuestions } from "../controllers";
+import { useQuestionsCount } from "../controllers";
 import { Outlet } from "react-router-dom";
 
 export const Quizz = () => {
-  const questions = useQuestions();
-  const initializeQuizz = useInitializeQuizz();
-  React.useEffect(() => {
-    initializeQuizz();
-  }, [initializeQuizz]);
+  const questionsCount = useQuestionsCount();
   return (
     <VStack spacing={8} w="100%" maxW="lg" justifySelf="center">
-      {questions.length === 0 ? (
+      {questionsCount === 0 ? (
         <VStack spacing={8} w="100%">
           <Heading as="span">Good luck, have fun!</Heading>
           <Text>The quizz will be ready soon</Text>
