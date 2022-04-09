e3f5376d50a69679409a47a6a702b3bdd66ffc21
Antoine Cheminat <ac.dev@hotmail.com>
2022-04-08 23:20:31 -0400

    Add Show correct answers button

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -1,13 +1,18 @@
 import * as React from "react";
-import { RepeatIcon } from "@chakra-ui/icons";
-import { Code, Heading, VStack } from "@chakra-ui/react";
+import { CheckCircleIcon, RepeatIcon } from "@chakra-ui/icons";
+import { ButtonGroup, Code, Heading, VStack } from "@chakra-ui/react";
 import { RouterButton } from "../components";
-import { useQuestionsCount, useScore } from "../controllers";
+import { useQuestionsCount, useScore, useSetQuizzStatus } from "../controllers";
 import { ROUTES } from "../navigation";
+import { To } from "react-router-dom";
 
 export const Results = () => {
   const score = useScore();
   const questionsCount = useQuestionsCount();
+  const setQuizzStatus = useSetQuizzStatus();
+  React.useEffect(() => {
+    setQuizzStatus("completed");
+  }, [setQuizzStatus]);
   return (
     <VStack spacing={8} w="100%" maxW="lg" justifySelf="center">
       <VStack spacing={4}>
@@ -21,9 +26,14 @@ export const Results = () => {
           {score}/{questionsCount}
         </Code>
       </VStack>
-      <RouterButton to={ROUTES.home} size="lg" rightIcon={<RepeatIcon />}>
-        Restart the quizz
-      </RouterButton>
+      <ButtonGroup size="lg">
+        <RouterButton to={ROUTES.home} rightIcon={<RepeatIcon />} variant="ghost">
+          Restart
+        </RouterButton>
+        <RouterButton to={-1 as To} rightIcon={<CheckCircleIcon />} colorScheme="green">
+          Show correct answers
+        </RouterButton>
+      </ButtonGroup>
     </VStack>
   );
 };
