8bf98bad6cd008e7f47537fcd4a6b9564a77984b
Antoine Cheminat <ac.dev@hotmail.com>
2022-04-08 23:18:23 -0400

    Display correct answer & disble radio buttons when quizz is completed

--- src/components/question/QuestionDisplay.tsx
+++ src/components/question/QuestionDisplay.tsx
@@ -1,18 +1,28 @@
-import { Heading, Radio, RadioGroup, Text, Wrap } from "@chakra-ui/react";
-import { useQuestionByIndex, useResultByIndex, useUpdateResults } from "../../controllers";
+import { Code, Heading, Radio, RadioGroup, Text, Wrap } from "@chakra-ui/react";
+import { useQuestionByIndex, useResultByIndex, useStatus, useUpdateResults } from "../../controllers";
 import { decodeHtml } from "../../helpers";
 
 export const QuestionDisplay = ({ index }: { index: number }) => {
   const updateResults = useUpdateResults();
   const result = useResultByIndex(index);
-  const { question, answers } = useQuestionByIndex(index);
+  const { question, answers, correct_answer } = useQuestionByIndex(index);
+  const quizzStatus = useStatus();
   return (
     <>
       <Heading as="h1" size="lg">
         Question n°{index + 1} :
       </Heading>
       <Text fontSize="xl">{decodeHtml(question)}</Text>
-      <RadioGroup value={result.answer} onChange={(value) => updateResults({ index, answer: value })} size="lg">
+      {quizzStatus === "completed" && (
+        <Code colorScheme="green" fontSize="xl">
+          Correct answer: {decodeHtml(correct_answer)}
+        </Code>
+      )}
+      <RadioGroup
+        value={result.answer}
+        onChange={(value) => updateResults({ index, answer: value })}
+        size="lg"
+        isDisabled={quizzStatus === "completed"}>
         <Wrap justify="center" spacing={4}>
           {answers ? (
             answers.map((value) => (
