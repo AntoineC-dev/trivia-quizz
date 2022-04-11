3d59b5b785badb7594e84415794ebfa1d352e014
Antoine Cheminat <ac.dev@hotmail.com>
2022-04-11 00:21:49 -0400

    Create Navbar & update grid template

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,14 +1,16 @@
 import { Grid, GridItem, Stack } from "@chakra-ui/react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { ROUTES } from "../../navigation";
 import { RouterButton } from "../router";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 
 export const Layout = () => {
+  const { key } = useLocation();
+
   return (
     <Grid minH="100vh" p={3} templateRows="auto 1fr" textAlign="center" gap={8}>
       <GridItem rowSpan={1} as={Stack} direction="row" justify="space-between">
-        <RouterButton to={ROUTES.home} variant="ghost">
+        <RouterButton to={ROUTES.home} variant="ghost" isDisabled={key === "default"}>
           Homepage
         </RouterButton>
         <ColorModeSwitcher />
