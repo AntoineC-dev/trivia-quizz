ff01ba11675da97ed551f83a52bfa7bd75e9526f
Antoine Cheminat <ac.dev@hotmail.com>
2022-04-10 23:47:57 -0400

    Update grid template layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,12 +1,19 @@
-import { Grid } from "@chakra-ui/react";
+import { Grid, GridItem, Stack } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
+import { ROUTES } from "../../navigation";
+import { RouterButton } from "../router";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 
 export const Layout = () => {
   return (
-    <Grid minH="100vh" p={3} textAlign="center">
-      <ColorModeSwitcher justifySelf="flex-end" />
-      <Outlet />
+    <Grid minH="100vh" p={3} templateRows="auto 1fr" textAlign="center" gap={8}>
+      <GridItem rowSpan={1} as={Stack} direction="row" justify="space-between">
+        <RouterButton to={ROUTES.home} variant="ghost">
+          Homepage
+        </RouterButton>
+        <ColorModeSwitcher />
+      </GridItem>
+      <GridItem rowSpan={1} as={Outlet} />
     </Grid>
   );
 };
