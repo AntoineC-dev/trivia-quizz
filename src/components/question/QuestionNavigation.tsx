01aeb560f9122430e344da211ee66a7f5ad1477c
Antoine Cheminat <ac.dev@hotmail.com>
2023-01-04 23:57:32 -0400

    Add unique key to RouterButton

--- src/components/question/QuestionNavigation.tsx
+++ src/components/question/QuestionNavigation.tsx
@@ -1,24 +1,25 @@
-import { Wrap } from "@chakra-ui/react";
-import { useQuestions, useResults, useStatus } from "../../controllers";
-import { ROUTES } from "../../navigation";
-import { RouterButton } from "../router";
+import { Wrap } from '@chakra-ui/react';
+import { useQuestions, useResults, useStatus } from '../../controllers';
+import { ROUTES } from '../../navigation';
+import { RouterButton } from '../router';
 
 export const QuestionNavigation = ({ index }: { index: number }) => {
   const questions = useQuestions();
   const quizzStatus = useStatus();
   const results = useResults();
   const buttonColorScheme = (i: number) => {
-    if (quizzStatus !== "completed") return "gray";
-    return results[i].isValid ? "green" : "red";
+    if (quizzStatus !== 'completed') return 'gray';
+    return results[i].isValid ? 'green' : 'red';
   };
   const buttonVariant = (i: number) =>
-    results[i].answer.length === 0 && quizzStatus !== "completed" ? "ghost" : "solid";
+    results[i].answer.length === 0 && quizzStatus !== 'completed' ? 'ghost' : 'solid';
 
   return (
     <Wrap justify="center">
       {questions.map((_, i) => (
         <RouterButton
           to={ROUTES.question(i)}
+          key={i}
           isActive={i === index}
           variant={buttonVariant(i)}
           colorScheme={buttonColorScheme(i)}>
